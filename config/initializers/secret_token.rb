080a8ab152ff75b7d980d88d48e6f1c06e967ae2
constzi <constantinz@yahoo.com>
2013-07-11 01:19:30 -0700

    1st trial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticker::Application.config.secret_token = '128c096b9e1fb706d2dfa8413a6f11c5ef1aa491d8e41117a700ba7edd4be7db2b7da74cd96d477eb3b23818fb1c2d00b21d5da0eff763a06ed1d1ff23f0d60d'
